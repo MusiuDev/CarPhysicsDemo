9fc24bec67427f3682458146b4aa39946cdcd087
MusiuDev <iszlonn@gmail.com>
2026-05-20 19:46:29 -0300

    Checkpoints and checkpoint groups

--- Assets/Scripts/Track/Checkpoint.cs
+++ Assets/Scripts/Track/Checkpoint.cs
@@ -1,16 +1,74 @@
+using System.Collections;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public delegate void CheckpointEvent(Checkpoint checkpoint);
+    public event CheckpointEvent OnCheckpointCompleted;
+
+    [SerializeField] private Animator _gateAnimator;
+    [SerializeField] private GameObject _colliderContainer;
+
+    private CheckpointState _currentState = CheckpointState.Unset;
+    private bool _isPlayerIn = false;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (!other.CompareTag("PlayerBody")) return;//TODO: Change this to something better.
+        Vector3 toPlayer = other.transform.position - this.transform.position;
+        bool enteredFromTheBack = Vector3.Dot(-this.transform.forward, toPlayer) > 0;
+        if (enteredFromTheBack)
+        {
+            _isPlayerIn = true;
+        }
+
+        if (_currentState != CheckpointState.Active) return;
+        SetState(CheckpointState.Open);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerExit(Collider other)
     {
-        
+        if (!other.CompareTag("PlayerBody")) return;//TODO: Change this to something better.
+
+        _isPlayerIn = false;
+
+        if (_currentState != CheckpointState.Open) return;
+
+        Vector3 toPlayer = other.transform.position - this.transform.position;
+        bool exitFromFromt = Vector3.Dot(this.transform.forward, toPlayer) > 0;
+        if (exitFromFromt)
+        {
+            SetState(CheckpointState.Completed);
+            OnCheckpointCompleted?.Invoke(this);
+        }
+        else
+        {
+            SetState(CheckpointState.Active);
+        }
+
+    }
+
+    public void SetState(CheckpointState newState)
+    {
+        if (_currentState == newState) return;
+
+        //If the player is already in when the checkpoint gets activated, open the gate instantly. 
+        if (_isPlayerIn && newState == CheckpointState.Active)
+        {
+            newState = CheckpointState.Open;
+        }
+
+        _currentState = newState;
+        _colliderContainer.SetActive((int)_currentState < (int)CheckpointState.Open);
+        _gateAnimator.SetInteger("State", (int)_currentState);
+    }
+
+    public enum CheckpointState
+    {
+        Unset = -1,
+        Blocked = 0,
+        Active = 1,
+        Open = 2,
+        Completed = 3,
     }
 }
--- Assets/Scripts/Track/CheckpointGroup.cs
+++ Assets/Scripts/Track/CheckpointGroup.cs
@@ -1,16 +1,93 @@
+using System.Collections;
 using UnityEngine;
 
 public class CheckpointGroup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public delegate void CheckpointGroupEvent(CheckpointGroup group);
+    public event CheckpointGroupEvent OnCheckpointGroupCleared;
+
+    [SerializeField] private Checkpoint[] _checkpoints;
+
+    private int _currentCheckpointIndex;
+    private Checkpoint _nextCheckpoint;
+    public bool Completed { get; private set; }
+
     void Start()
     {
-        
+        Reset();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Reset()
     {
-        
+        SubscribeToCheckpoints();
+        for (int i = 0; i < _checkpoints.Length; i++)
+        {
+            _checkpoints[i].SetState(Checkpoint.CheckpointState.Blocked);
+        }
+        _currentCheckpointIndex = 0;
+        Completed = false;
+        UpdateNextCheckpoint();
+    }
+
+    void OnDestroy()
+    {
+        UnsubscribeFromCheckpoints();
+    }
+
+
+    private void HandleCheckpointCompleted(Checkpoint checkpoint)
+    {
+        if (checkpoint != _nextCheckpoint || Completed)
+        {
+            Debug.LogError("Invalid Checkpoint Completion. This should have never happened");
+            return;
+        }
+
+        _currentCheckpointIndex++;
+        if (_currentCheckpointIndex >= _checkpoints.Length)
+        {
+            MarkAsCompleted();
+        }
+        else
+        {
+            UpdateNextCheckpoint();
+        }
+    }
+
+    private void UpdateNextCheckpoint()
+    {
+        _nextCheckpoint = _checkpoints[_currentCheckpointIndex];
+        _nextCheckpoint.SetState(Checkpoint.CheckpointState.Active);
+    }
+
+    private void MarkAsCompleted()
+    {
+        Completed = true;
+        OnCheckpointGroupCleared?.Invoke(this);
+        UnsubscribeFromCheckpoints();
+        StartCoroutine(WaitAndReset());
+    }
+
+    private void SubscribeToCheckpoints()
+    {
+        UnsubscribeFromCheckpoints();
+        for (int i = 0; i < _checkpoints.Length; i++)
+        {
+            _checkpoints[i].OnCheckpointCompleted += HandleCheckpointCompleted;
+        }
+    }
+
+    private void UnsubscribeFromCheckpoints()
+    {
+        for (int i = 0; i < _checkpoints.Length; i++)
+        {
+            _checkpoints[i].OnCheckpointCompleted -= HandleCheckpointCompleted;
+        }
+    }
+
+    IEnumerator WaitAndReset()
+    {
+        yield return new WaitForSeconds(2f);
+        Reset();
     }
 }
