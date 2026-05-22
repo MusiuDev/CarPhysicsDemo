5afefd8b53d7aa07622af0df8dbd113f600df8fb
MusiuDev <iszlonn@gmail.com>
2026-05-22 19:13:55 -0300

    editor script cleanup

--- Assets/Scripts/Editor/CheckpointGroupEditor.cs
+++ Assets/Scripts/Editor/CheckpointGroupEditor.cs
@@ -9,24 +9,29 @@ public class CheckpointGroupEditor : Editor
         base.OnInspectorGUI();
         if (GUILayout.Button("Auto Assign Checkpoints"))
         {
-            CheckpointGroup group = target as CheckpointGroup;
-            if (!group) return;
-
-            Checkpoint[] childCheckpoints = group.gameObject.GetComponentsInChildren<Checkpoint>();
-            if (childCheckpoints == null || childCheckpoints.Length == 0) return;
-
-            serializedObject.Update();
-            
-            SerializedProperty checkpointsProp = serializedObject.FindProperty("_checkpoints");
-            checkpointsProp.arraySize = childCheckpoints.Length;
-
-            for (int i = 0; i < childCheckpoints.Length; i++)
-            {
-                SerializedProperty chk = checkpointsProp.GetArrayElementAtIndex(i);
-                chk.objectReferenceValue = childCheckpoints[i];
-            }
-            serializedObject.ApplyModifiedProperties();
+            AutoAssignCheckpoints();
         }
     }
 
+
+    private void AutoAssignCheckpoints()
+    {
+        CheckpointGroup group = target as CheckpointGroup;
+        if (!group) return;
+
+        Checkpoint[] childCheckpoints = group.gameObject.GetComponentsInChildren<Checkpoint>();
+        if (childCheckpoints == null || childCheckpoints.Length == 0) return;
+
+        serializedObject.Update();
+
+        SerializedProperty checkpointsProp = serializedObject.FindProperty("_checkpoints");
+        checkpointsProp.arraySize = childCheckpoints.Length;
+
+        for (int i = 0; i < childCheckpoints.Length; i++)
+        {
+            SerializedProperty chk = checkpointsProp.GetArrayElementAtIndex(i);
+            chk.objectReferenceValue = childCheckpoints[i];
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
 }
