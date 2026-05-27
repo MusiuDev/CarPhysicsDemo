81f807ea167555ffb161c091ef7155b98e9a7773
MusiuDev <iszlonn@gmail.com>
2026-05-27 00:54:35 -0300

    Added touch inputs

--- Assets/Scripts/Player/CarInputController.cs
+++ Assets/Scripts/Player/CarInputController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class CarInputController : MonoBehaviour
 {
@@ -9,10 +10,21 @@ public class CarInputController : MonoBehaviour
     [SerializeField] private InputActionReference _brakeActionRef;
     [SerializeField] private bool _autoAccelerate;
     [SerializeField] private bool _inputEnabledAtStart;
+    [SerializeField] private UIDocument _inputHUD;
+    [SerializeField] private bool _forceTouchInputs;
 
     private InputAction _steerAction;
     private InputAction _accelerateAction;
     private InputAction _brakeAction;
+
+    private VisualElement _steerLeftUIButton;
+    private VisualElement _steerRightUIButton;
+    private VisualElement _accelerateUIButton;
+    private VisualElement _brakeUIButton;
+
+    private float _uiSteer;
+    private bool _uiAccel;
+    private bool _uiBrake;
 
     private bool _inputEnabled;
     public bool InputEnabled
@@ -51,21 +63,77 @@ public class CarInputController : MonoBehaviour
         {
             InputEnabled = true;
         }
+        SetupUIInputs();
+
+    }
+
+    private void SetupUIInputs()
+    {
+        if (!_inputHUD) return;
+        VisualElement root = _inputHUD.rootVisualElement;
+
+        bool hasTouch = Touchscreen.current != null;
+        if (!hasTouch && !_forceTouchInputs)
+        {
+            VisualElement inputContainer = root.Query<VisualElement>("InputButtonsContainer");
+            if (inputContainer != null) inputContainer.style.display = DisplayStyle.None;
+            return;
+        }
+
+        _steerLeftUIButton = root.Query<VisualElement>("SteerLeft_Btn");
+        _steerRightUIButton = root.Query<VisualElement>("SteerRight_Btn");
+        _accelerateUIButton = root.Query<VisualElement>("Aceelerate_Btn");
+        _brakeUIButton = root.Query<VisualElement>("Brake_Btn");
+
+        if (_steerLeftUIButton != null)
+        {
+            _steerLeftUIButton.RegisterCallback<PointerDownEvent>(_ => _uiSteer = -1f, TrickleDown.TrickleDown);
+            _steerLeftUIButton.RegisterCallback<PointerUpEvent>(_ => _uiSteer = 0);
+            _steerLeftUIButton.RegisterCallback<PointerLeaveEvent>(_ => _uiSteer = 0);
+            _steerLeftUIButton.RegisterCallback<PointerCancelEvent>(_ => _uiSteer = 0);
+        }
+
+        if (_steerRightUIButton != null)
+        {
+            _steerRightUIButton.RegisterCallback<PointerDownEvent>(_ => _uiSteer = 1f, TrickleDown.TrickleDown);
+            _steerRightUIButton.RegisterCallback<PointerUpEvent>(_ => _uiSteer = 0);
+            _steerRightUIButton.RegisterCallback<PointerLeaveEvent>(_ => _uiSteer = 0);
+            _steerRightUIButton.RegisterCallback<PointerCancelEvent>(_ => _uiSteer = 0);
+        }
+
+        if (_accelerateUIButton != null)
+        {
+            _accelerateUIButton.RegisterCallback<PointerDownEvent>(_ => _uiAccel = true, TrickleDown.TrickleDown);
+            _accelerateUIButton.RegisterCallback<PointerUpEvent>(_ => _uiAccel = false);
+            _accelerateUIButton.RegisterCallback<PointerLeaveEvent>(_ => _uiAccel = false);
+            _accelerateUIButton.RegisterCallback<PointerCancelEvent>(_ => _uiAccel = false);
+        }
+
+        if (_brakeUIButton != null)
+        {
+            _brakeUIButton.RegisterCallback<PointerDownEvent>(_ => _uiBrake = true, TrickleDown.TrickleDown);
+            _brakeUIButton.RegisterCallback<PointerUpEvent>(_ => _uiBrake = false);
+            _brakeUIButton.RegisterCallback<PointerLeaveEvent>(_ => _uiBrake = false);
+            _brakeUIButton.RegisterCallback<PointerCancelEvent>(_ => _uiBrake = false);
+        }
     }
 
     void Update()
     {
         if (!InputEnabled) return;
 
-        float steer = _steerAction.ReadValue<float>();
-        bool accelerate = AutoAccelerate || (_accelerateAction.ReadValue<float>() > 0.5f);
-        bool brake = _brakeAction.ReadValue<float>() > 0.5f;
+        float steer = Mathf.Clamp((_steerAction.ReadValue<float>() + _uiSteer), -1, 1);
+        bool accelerate = AutoAccelerate || (_accelerateAction.ReadValue<float>() > 0.5f) || _uiAccel;
+        bool brake = _brakeAction.ReadValue<float>() > 0.5f || _uiBrake;
 
         _car.SetInput(steer, accelerate, brake);
     }
 
     private void ResetInputState()
     {
+        _uiAccel = false;
+        _uiBrake = false;
+        _uiSteer = 0f;
         _car.SetInput(0f, false, false);
     }
 }
